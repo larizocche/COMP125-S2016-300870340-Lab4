0bd02aa6001923a00d73a8234cdb1b09f8d6e683
larizocche <lari.zocche@gmail.com>
2016-07-07 17:51:51 -0400

    created new user object

--- Scripts/app.js
+++ Scripts/app.js
@@ -13,34 +13,54 @@
 (function () {
     "use strict";
 
-  
+    //Class User
+    var User = (function () {
+        //constructor
+        function User(username, password) {
+            this._userName = username;
+            this._password = password;
+        }
+        //Set Username
+        User.prototype.SetUserName = function (username) {
+            this._userName = username;
+        };
+        //Get Username
+        User.prototype.GetUserName = function () {
+            return this._userName;
+        };
+        //Set Password
+        User.prototype.SetPassword = function (password) {
+            this._password = password;
+        };
+        //Get Password
+        User.prototype.GetPassword = function () {
+            return this._password;
+        };
+        return User;
+    } ());
 
-
-  var User = {
-      ID: "1",
-      Name: "admin",
-      Email: "admin@example.com",
-      Password: "12345"
-
-  };
 
     function init() {
         console.log("Application Started");
-        console.log(User);
-        console.log(User.Name);
-    
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("pwd").value;
 
+        var user = new User(username, password);
+
+        console.log("Username: " + user._userName);
+        console.log("Password: " + user._password);
     }
 
-    
 
-   
+    var pwd = document.getElementById("pwd");
 
+    var contactForm = document.getElementById("contactForm");
+    contactForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        init();
+        contactForm.reset();
 
-
-
-    // call init funciton when window finishes loading
-    window.addEventListener("load", init);
-
+    });
 
 })();
+
